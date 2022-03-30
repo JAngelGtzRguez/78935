c8d44907236b9c8daaa6d9c1f9516f73a183f738
JAngelGtzRguez <angel.rogu017@gmail.com>
2022-03-30 17:16:54 -0600

    Correcion de Issue y anexo la actividad de Saludos

---
+++ Saludos/src/main/java/mx/uv/t4js/Saludos/Saludo.java
@@ -0,0 +1,34 @@
+package mx.uv.t4js.Saludos;
+
+public class Saludo {
+    private int id;
+    private String nombre;
+
+    public Saludo(){
+
+    }
+
+    public Saludo(int id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    
+}
